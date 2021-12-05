f9da763870cb97b7c8d84e0c03b7fec83854eaf8
no backsies <67820669+Lambdarevolution@users.noreply.github.com>
2021-12-05 23:10:12 +0100

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -6,3 +6,16 @@
 #include <string>
 #include <iostream>
 #include <vector>
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+  switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+      _beginthreadex(NULL, 0, &OnLoad, NULL, 0, NULL); // Run in a new thread to not hog the main.
+      break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
