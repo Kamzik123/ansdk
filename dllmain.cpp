2f40a1fcf6c002a2096f594e1b618c308a9e9d4a
no backsies <67820669+Lambdarevolution@users.noreply.github.com>
2021-12-05 23:09:19 +0100

    Create dllmain.cpp

---
+++ dllmain.cpp
@@ -0,0 +1,8 @@
+// Created by Lambda
+// dllmain.cpp : Defines the entry point for the DLL application.
+
+#include "pch.h"
+#include "memory.h"
+#include <string>
+#include <iostream>
+#include <vector>
