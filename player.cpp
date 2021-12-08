4ef1e5db96b9b2277ce82345eb906a7604f1213f
no backsies <67820669+Lambdarevolution@users.noreply.github.com>
2021-12-08 11:15:27 +0100

    Update player.cpp

--- player.cpp
+++ player.cpp
@@ -1,6 +1,6 @@
 namespace lambdasdk {
   namespace player {
-
+    
     // Returns the current health (0-200)
     // No Need for an internal function we use pointers to the address right away instead.
     int GetHealth() {
