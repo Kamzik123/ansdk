888f04c8d9e9e4bd2a7dfda15267e5fe5eceb2b8
no backsies <67820669+Lambdarevolution@users.noreply.github.com>
2021-12-17 10:40:33 +0100

    Update Offsets.h

--- Offsets.h
+++ Offsets.h
@@ -8,8 +8,10 @@
 #define OFFSETS_H
 
 namespace Offsets {
-  // Pattern, mask..problaby a cleaner way doing this but whatever...
-  static const char* const ExitProcess[] =   { "\xE8\x00\x00\x00\x00\x83\xC4\x04\xFF\x15\x00\x00\x00\x00\x8A\xD8", "x????xxxxx????xx" };
-  static const char* const GetWeaponName[] = { "\x55\x8B\xEC\x8B\x45\x08\x83\xF8\x22", "xxxxxxxxx" };
+  // Pattern, mask..there is problaby a cleaner way doing this but whatever...
+  static const char* const ExitProcess[] =       { "\xE8\x00\x00\x00\x00\x83\xC4\x04\xFF\x15\x00\x00\x00\x00\x8A\xD8", "x????xxxxx????xx" };
+  static const char* const GetWeaponName[] =     { "\x55\x8B\xEC\x8B\x45\x08\x83\xF8\x22", "xxxxxxxxx" };
+  static const char* const SetPlayerHealth[] =   { "\x55\x8B\xEC\x8B\x45\x08\x8B\x50\x0C\x8B\x40\x10", "xxxxxxxxxxxx" };
+
 }
 #endif
