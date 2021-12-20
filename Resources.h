90ac2866dae3376615f66537c866c5ac697716b7
no backsies <67820669+Lambdarevolution@users.noreply.github.com>
2021-12-20 16:18:18 +0100

    Update Resources.h

--- Resources.h
+++ Resources.h
@@ -1,4 +1,14 @@
-
+/*
+| License: Unlicense
+| Created by: Lambdarevolution(at)gmail.com / 2020-04-01
+| Description: Just a contatiner class for resource.
+*/
+#ifndef RESOURCES_H
+#define RESOURCES_H
 class Resources {
 public:
+ resource money;
+
 };
+
+#endif // RESOURCES_H
