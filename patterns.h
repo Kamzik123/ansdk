b99f03786e4b57fe513e78e64fa2a674cc051b41
Lambdarevolution <67820669+Lambdarevolution@users.noreply.github.com>
2022-01-04 12:40:47 +0100

    Update and rename Offsets.h to patterns.h

---
+++ patterns.h
@@ -0,0 +1,18 @@
+/*
+| License: Unlicense
+| Created by: Lambdarevolution(at)gmail.com / 2021-12-01
+| Description: patterns, not final...
+*/
+#ifndef PATTERNS_H
+#define PATTERNS_H
+
+namespace Patterns {
+// Will add them soon...
+
+}   
+
+namespace Pointers {
+ static const int Player[] { 0x3C, 0xE8, 0x1C, 0x5C, 0x0, 0xC, 0x0, 0x0 }; 
+ static const int PlayerShip[]{ 0x4, 0x4, 0xC, 0x4C, 0x38, 0xC, 0x2C, 0x208 }; 
+}
+#endif
